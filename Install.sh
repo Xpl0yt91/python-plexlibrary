168f7fec2a90939281af1d14238eb0122c386001
Xployt <43792836+Xpl0yt91@users.noreply.github.com>
2021-05-12 16:43:53 +0100

    Layout
    Just quick layout for install

--- Install.sh
+++ Install.sh
@@ -1,25 +1,55 @@
 #!/bin/bash
 ######################################################
-# All rights reserved.                               #
+# Title:  SudoBox                                    #
 # started from Adamgot                               #
-# Install script from SudoBox.io                     #
+# URL: https://sudobox.io                            #
 ######################################################
 
-Plex.Token=$(sudo cat "/opt/appdata/plex/database/Library/Application Support/Plex Media Server/Preferences.xml" | sed -e 's;^.* PlexOnlineToken=";;' | sed -e 's;".*$;;' | tail -1)
+#variable recalls
+cat "/opt/appdata/plex/database/Library/Application Support/Plex Media Server/Preferences.xml" | sed -e 's;^.* PlexOnlineToken=";;' | sed -e 's;".*$;;' | tail -1 >> /opt/sudobox/var/plex.token
+plexlibrary=(/opt/sudobox/python-plexlibrary)
+plex.token=(cat opt/sudobox/var/plex.token)
+
+#Checks Plex has been installed and is running
+
+plexcheck() {
+  pcheck=$(docker ps --format '{{.Names}}' | grep "plex")
+  if [ "$pcheck" == "" ]; then
+
+    tee <<-EOF
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+⛔️  WARNING! - Plex is not Installed/Running! Cannot Proceed!
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
+    read -p 'Confirm Info | PRESS [ENTER] ' typed </dev/tty
+    question1
+  fi
+}
+
+#check Trakt api client file exist if it does then copy to
+if [[ -f /var/plexguide/traktarr/trakt.client ]]
+then
+    traktid$(cat /var/plexguide/traktarr/trakt.client)
+    echo "<file> exists on your filesystem"
+fi
+
+#check Trakt api secret file exist if it does then copy to
+if [[ -f /var/plexguide/traktarr/trakt.secret ]]
+then
+    traktid$(cat /var/plexguide/traktarr/trakt.secret)
+    echo "<file> exists on your filesystem"
+fi
+
 
 #### create folders & set permissions
 USER=$(getent passwd 1000 | awk -F: '{ print $1}')
 
-mkdir -p opt/appdata/python-plexlibrary
+mkdir -p /opt/sudobox/var
+mkdir -p /opt/sudobox/python-plexlibrary
 
-#gitpull to directory
+#Download and put required files into the correct directory
+git clone -b plex-movie-agent --single-branch https://github.com/Xpl0yt91/python-plexlibrary.git $plexlibrary
 
 #install requirements
+pip install -rv $plexlibrary/requirements.txt
 
-#get plex token and more
-
-
-
-#copy apis and details
-
-#give options to install things automagically - if this then that function
